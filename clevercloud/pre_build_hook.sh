1209718b06917917b89f05521a96aabc6f23168c
pmontens <pieterjan@montens.net>
2020-06-16 15:17:22 +0200

    :wrench: fixing deployment

--- clevercloud/pre_build_hook.sh
+++ clevercloud/pre_build_hook.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 echo "Executing prepare.sh clevercloud init script"
-cp /clevercloudinit/parameters.yml app/config/parameters.yml
+cp ./clevercloud/init/parameters.yml ./app/config/parameters.yml
