7898a31357ebf6c5c5ee87a574df9c92afda21d7
pmontens <pieterjan@montens.net>
2020-06-16 17:08:20 +0200

    :wrench: fixing deployment

--- clevercloud/pre_build_hook.sh
+++ clevercloud/pre_build_hook.sh
@@ -6,6 +6,6 @@ cp ./clevercloud/init/parameters.yml ./app/config/parameters.yml
 var_list="MYSQL_ADDON_HOST MYSQL_ADDON_PORT MYSQL_ADDON_DB MYSQL_ADDON_USER MYSQL_ADDON_PASSWORD MONGODB_ADDON_DB MONGODB_ADDON_URI"
 for key in $var_list; do
     echo "Setting $key in parameters.yml"
-    sed -e -i 's|%'"$key"'%|'"${!key}"'|' ./app/config/parameters.yml
+    sed -i -e 's|%'"$key"'%|'"${!key}"'|' ./app/config/parameters.yml
 done
 cat ./app/config/parameters.yml
